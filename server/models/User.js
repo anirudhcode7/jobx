676b5457d6f82591a0b16876389ef21c15676c57
anirudh.ponna <anirudh.ponna@appviewx.com>
2023-11-01 13:36:23 +0530

    Added User schema for Mongo DB with salt and hashing

---
+++ server/models/User.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+const crypto = require('crypto'); // For generating the salt and hashing the password
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  hashed_password: {
+    type: String,
+    required: true,
+  },
+  salt: {
+    type: String,
+    required: true,
+  },
+});
+
+userSchema
+  .virtual('password')
+  .set(function (password) {
+    this._password = password;
+    if (!this.salt) {
+        this.salt = crypto.randomBytes(16).toString('hex');
+    }
+    this.hashed_password = this.encryptPassword(password);
+  })
+  .get(function () {
+    return this._password;
+  });
+
+userSchema.methods = {
+  // Method to hash the password using the user's unique salt
+  encryptPassword: function (password) {
+    if (!password || !this.salt) return '';
+    try {
+      return crypto
+        .createHmac('sha1', this.salt)
+        .update(password)
+        .digest('hex');
+    } catch (err) {
+      return '';
+    }
+  },
+
+  // Method to authenticate the user by comparing the hashed password
+  authenticate: function (plainText) {
+    return this.encryptPassword(plainText) === this.hashed_password;
+  },
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
