bf9c98c148f1b358b367dede5d6efb83b1ea5b41
DivakarSai <divakarsainukala@gmail.com>
2024-04-12 15:15:05 +0530

    seeach bar added on ui

--- client/src/components/core/JobsMain.js
+++ client/src/components/core/JobsMain.js
@@ -81,16 +81,48 @@ export default function JobsMain() {
     <>
       <div className="p-6">
         <h1 className="text-xl font-bold text-black mb-4">All Jobs</h1>
-        {userInfo?.role === "admin" && (
-          <Button
-            onPress={onOpen}
-            className="ml-auto text-white bg-indigo-800"
-            size="lg"
-            radius="full"
-            variant="bordered"
-          >
-            Add Job
-          </Button>
+        <div className="flex items-center justify-between mb-4">
+          {userInfo?.role === "admin" && (
+            <Button
+              onPress={onOpen}
+              className="text-white bg-indigo-800"
+              size="lg"
+              radius="full"
+              variant="bordered"
+            >
+              Add Job
+            </Button>
+          )}
+          <input
+            type="text"
+            placeholder="Search"
+            className="ml-4 px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500 w-72" // Increase the width of the search bar by adding 'w-72' class
+          />
+        </div>
+
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          jobs
+            .slice(0)
+            .reverse()
+            .map((job, index) => (
+              <JobPostMain
+                key={index}
+                id={job._id.toString()}
+                title={job.title}
+                jobLink={job.job_link}
+                companyLogoUrl={job.company_logo}
+                company={job.company_name}
+                description={job.description}
+                location={job.location}
+                employmentType={job.employment_type}
+                yearsOfExperience={job.experience_required}
+                skills={job.skills_required}
+                handleDelete={userInfo?.role === "admin" ? handleDelete : null}
+                handleEdit={userInfo?.role === "admin" ? handleEdit : null}
+              />
+            ))
         )}
 
         <AddJobModal
@@ -100,28 +132,6 @@ export default function JobsMain() {
           initialJobData={jobData}
           onUpdateJob={handleUpdateJob}
         />
-
-        {loading ? (
-          <p>Loading...</p>
-        ) : (
-          jobs.slice(0).reverse().map((job, index) => (
-            <JobPostMain
-              key={index}
-              id={job._id.toString()}
-              title={job.title}
-              jobLink={job.job_link}
-              companyLogoUrl={job.company_logo}
-              company={job.company_name}
-              description={job.description}
-              location={job.location}
-              employmentType={job.employment_type}
-              yearsOfExperience={job.experience_required}
-              skills={job.skills_required}
-              handleDelete={userInfo?.role === "admin" ? handleDelete : null}
-              handleEdit={userInfo?.role === "admin" ? handleEdit : null}
-            />
-          ))
-        )}
       </div>
     </>
   );
